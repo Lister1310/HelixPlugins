f78bbdd359f69b62800dfadbc8e2e044a2c85b1d
Lister <57773281+Lister1310@users.noreply.github.com>
2021-09-10 00:31:45 +0300

    Plugins uploaded to github!

---
+++ d-flag.lua
@@ -0,0 +1,19 @@
+PLUGIN.name = "D-Flag"
+PLUGIN.description = "The flag under which the character will not be banned forever after death."
+PLUGIN.author = "Lister"
+
+ix.flag.Add("D", "The flag at which the character will not be banned forever.")
+
+ix.config.SetDefault("permakill", true, "Whether or not permakill is activated on the server.", nil, {
+    category = "Permakill"
+})
+
+ix.config.SetDefault("permakillWorld", true, "Whether or not world and self damaged produce permanent death.", nil, {
+    category = "Permakill"
+})
+
+function PLUGIN:ShouldPermakillCharacter(client, character, inflictor, attacker)
+    if character:HasFlags("D") then
+        return false
+    end
+end
