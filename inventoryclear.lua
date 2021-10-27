99362249e4b32c1bb7480a4c5b96ea443898ccd0
Lister <57773281+Lister1310@users.noreply.github.com>
2021-10-27 10:12:24 +0300

    Update inventoryclear.lua

--- inventoryclear.lua
+++ inventoryclear.lua
@@ -11,15 +11,15 @@ ix.config.Add("Clear Equipped", false, "Clear only equipped items after characte
 })
 
 function PLUGIN:PlayerDeath(client)
-    if ix.config.Get("Clear Inventory", true) then
-        for k, v in pairs(client:GetCharacter():GetInventory():GetItems()) do
-            v:Remove()
-	end
-    elseif ix.config.Get("Clear Equipped", true) then
-	for a, b in pairs(client:GetCharacter():GetInventory():GetItems()) do
-	    if b:GetData("equip") == true then
-                b:Remove()
-            end
+	if ix.config.Get("Clear Inventory", true) then
+		for k, v in pairs(client:GetCharacter():GetInventory():GetItems()) do
+			v:Remove()
+		end
+	elseif ix.config.Get("Clear Equipped", true) then
+		for a, b in pairs(client:GetCharacter():GetInventory():GetItems()) do
+			if b:GetData("equip") == true then
+            	b:Remove()
+        	end
         end
-    end
+	end
 end
