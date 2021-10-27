88add12d24c95289b4bfcf9787c18148f9efa74e
Lister <57773281+Lister1310@users.noreply.github.com>
2021-10-27 10:06:21 +0300

    Add files via upload

---
+++ inventoryclear.lua
@@ -0,0 +1,25 @@
+PLUGIN.name = "Inventory clear"
+PLUGIN.description = "Clear inventory after character death"
+PLUGIN.author = "Lister"
+
+ix.config.Add("Clear Inventory", false, "Clear all Inventory items after character death?", nil, {
+	category = "Inventory"
+})
+
+ix.config.Add("Clear Equipped", false, "Clear only equipped items after character death?", nil, {
+	category = "Inventory"
+})
+
+function PLUGIN:PlayerDeath(client)
+	if ix.config.Get("Clear Inventory", true) then
+		for k, v in pairs(client:GetCharacter():GetInventory():GetItems()) do
+			v:Remove()
+		end
+	elseif ix.config.Get("Clear Equipped", true) then
+		for a, b in pairs(client:GetCharacter():GetInventory():GetItems()) do
+			if b:GetData("equip") == true then
+            	b:Remove()
+        	end
+        end
+	end
+end
