5a5cef45e7fbd905d041c56526027120ebcb4f08
Lister <57773281+Lister1310@users.noreply.github.com>
2021-12-24 00:10:18 +0200

    Update sv_cardealer.lua

--- ixcardealer/derma/sv_cardealer.lua
+++ ixcardealer/derma/sv_cardealer.lua
@@ -1,15 +1,20 @@
 util.AddNetworkString("CarDealerUsed")
 util.AddNetworkString("ixMoneyTake")
 net.Receive("ixMoneyTake", function(len, player)
-    local id = net.ReadInt(32)
+    local id = net.ReadUInt(32)
     local CarData = IxCarDealer.Cars[id]
+        
+    if !CarData then
+        return
+    end
+        
     local pricecar = tonumber(CarData.price)
     local character = player:GetCharacter()
     local wallet = character:GetMoney()
     local spawnname = CarData.spawnname
     local spwn = CarData.spawnpos
     local ang = CarData.angle
-
+        
     if pricecar > wallet then
         return player:Notify("You don't have enough funds to rent a car!")
     end
