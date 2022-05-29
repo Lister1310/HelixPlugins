e15f248aff1c2b7b238e839dc448ab1958a618f5
Lister <57773281+Lister1310@users.noreply.github.com>
2022-05-29 19:31:01 +0300

    Update ix_cardealer.lua

--- ixcardealer/entities/entities/ix_cardealer.lua
+++ ixcardealer/entities/entities/ix_cardealer.lua
@@ -6,30 +6,34 @@ ENT.Base = "base_ai"
 ENT.Spawnable = true
 ENT.AdminOnly = true
 
-if SERVER then
-	function ENT:Initialize()
-		self:SetModel("models/Combine_Soldier.mdl")
-	    self:SetSolid(SOLID_BBOX)
-	    self:SetUseType(SIMPLE_USE)
-	   	self:SetCollisionGroup(COLLISION_GROUP_NONE)
+if (SERVER) then
+    function ENT:Initialize()
+        self:SetModel("models/Combine_Soldier.mdl")
+	self:SetSolid(SOLID_BBOX)
+	self:SetUseType(SIMPLE_USE)
+	self:SetCollisionGroup(COLLISION_GROUP_NONE)
         self:SetMoveType(MOVETYPE_VPHYSICS)
-	end
+    end
 
-	function ENT:Use(client)	
-		net.Start("CarDealerUsed")
+    function ENT:Use(client)	
+    	local character = client:GetCharacter()
+	
+	if (client and character) then	
+	    net.Start("CarDealerUsed")
 	    net.Send(client)
+	end
     end
 end
 
-if CLIENT then
-	function ENT:OnPopulateEntityInfo(container)
-	   local cardealername = container:AddRow("cardealername")
-	   cardealername:SetImportant()
-	   cardealername:SetText(L("cardealerName"))
-	   cardealername:SizeToContents()
+if (CLIENT) then
+    function ENT:OnPopulateEntityInfo(container)
+	local carDealerName = container:AddRow("carDealerName")
+	carDealerName:SetImportant()
+	carDealerName:SetText(L("cardealerName"))
+	carDealerName:SizeToContents()
 
-	   local cardealerdesc = container:AddRow("cardealerdesc")
-	   cardealerdesc:SetText(L("cardealerDesc"))
-	   cardealerdesc:SizeToContents()
+	local carDealerDesc = container:AddRow("carDealerDesc")
+	carDealerDesc:SetText(L("cardealerDesc"))
+	carDealerDesc:SizeToContents()
     end
 end
