b1b0a8edcf42cc3fcdc16f10c00f5689db960695
Lister <57773281+Lister1310@users.noreply.github.com>
2022-05-29 19:34:05 +0300

    Update sv_cardealer.lua

--- ixcardealer/derma/sv_cardealer.lua
+++ ixcardealer/derma/sv_cardealer.lua
@@ -2,26 +2,24 @@ util.AddNetworkString("CarDealerUsed")
 util.AddNetworkString("ixMoneyTake")
 net.Receive("ixMoneyTake", function(len, player)
     local id = net.ReadUInt(32)
-    local CarData = IxCarDealer.Cars[id]
+    local carData = IxCarDealer.Cars[id]
         
-    if !CarData then
-        return
-    end
+    if (!carData) then return end
         
-    local pricecar = tonumber(CarData.price)
+    local priceCar = tonumber(carData.price)
     local character = player:GetCharacter()
     local wallet = character:GetMoney()
-    local spawnname = CarData.spawnname
-    local spwn = CarData.spawnpos
-    local ang = CarData.angle
+    local spawnName = carData.spawnname
+    local spwn = carData.spawnpos
+    local ang = carData.angle
         
-    if pricecar > wallet then
+    if (priceCar > wallet) then
         return player:Notify("You don't have enough funds to rent a car!")
     end
 
-    character:TakeMoney(pricecar)
-    player:Notify("You have buy successfully "..CarData.name.." for "..pricecar..ix.currency.symbol.."!")
+    character:TakeMoney(priceCar)
+    player:Notify("You have buy successfully "..carData.name.." for "..priceCar.." "..ix.currency.symbol.."!")
 
-    simfphys.SpawnVehicleSimple(spawnname, spwn, ang)
+    simfphys.SpawnVehicleSimple(spawnName, spwn, ang)
     simfphys.EntityOwner = player
 end)
